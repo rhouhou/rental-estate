33b7534542bd968e66fde8378ceaaa320cfdbb14
Rola Houhou <rola.h.houhou@gmail.com>
2026-07-09 16:14:27 +0200

    Clean user controller logic

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,45 +1,56 @@
-import bcrypt from "bcryptjs";
+import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
-import { errorHandler } from "../utils/error.js";
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const test = (req, res) => {
-  res.send("Test route being called");
+  res.status(200).json({
+    message: "User route is working",
+  });
 };
 
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.id) {
-    return next(errorHandler(401, "You can only update your own account"));
+    return next(errorHandler(403, "You can only update your own account"));
   }
 
   try {
+    const updateData = {};
+
+    if (req.body.username) {
+      updateData.username = req.body.username;
+    }
+
+    if (req.body.email) {
+      updateData.email = req.body.email;
+    }
+
+    if (req.body.avatar) {
+      updateData.avatar = req.body.avatar;
+    }
+
     if (req.body.password) {
-      req.body.password = bcrypt.hashSync(req.body.password, 10);
+      if (req.body.password.length < 6) {
+        return next(errorHandler(400, "Password must be at least 6 characters"));
+      }
+
+      updateData.password = await bcryptjs.hash(req.body.password, 10);
     }
 
     const updatedUser = await User.findByIdAndUpdate(
       req.params.id,
-      {
-        $set: {
-          username: req.body.username,
-          email: req.body.email,
-          password: req.body.password,
-          avatar: req.body.avatar,
-        },
-      },
+      { $set: updateData },
       {
         new: true,
         runValidators: true,
       }
-    );
+    ).select("-password");
 
     if (!updatedUser) {
       return next(errorHandler(404, "User not found"));
     }
 
-    const { password, ...rest } = updatedUser._doc;
-
-    return res.status(200).json(rest);
+    return res.status(200).json(updatedUser);
   } catch (error) {
     next(error);
   }
@@ -47,7 +58,7 @@ export const updateUser = async (req, res, next) => {
 
 export const deleteUser = async (req, res, next) => {
   if (req.user.id !== req.params.id) {
-    return next(errorHandler(401, "You can only delete your own account"));
+    return next(errorHandler(403, "You can only delete your own account"));
   }
 
   try {
@@ -57,9 +68,12 @@ export const deleteUser = async (req, res, next) => {
       return next(errorHandler(404, "User not found"));
     }
 
-    res.clearCookie("access_token");
+    await Listing.deleteMany({ userRef: req.params.id });
 
-    return res.status(200).json("User has been deleted");
+    return res
+      .clearCookie("access_token")
+      .status(200)
+      .json("User account and listings have been deleted");
   } catch (error) {
     next(error);
   }
@@ -67,11 +81,14 @@ export const deleteUser = async (req, res, next) => {
 
 export const getUserListings = async (req, res, next) => {
   if (req.user.id !== req.params.id) {
-    return next(errorHandler(401, "You can only view your own listings"));
+    return next(errorHandler(403, "You can only view your own listings"));
   }
 
   try {
-    const listings = await Listing.find({ userRef: req.params.id });
+    const listings = await Listing.find({ userRef: req.params.id })
+      .sort({ createdAt: -1 })
+      .select("-__v")
+      .lean();
 
     return res.status(200).json(listings);
   } catch (error) {
@@ -81,15 +98,13 @@ export const getUserListings = async (req, res, next) => {
 
 export const getUser = async (req, res, next) => {
   try {
-    const user = await User.findById(req.params.id);
+    const user = await User.findById(req.params.id).select("-password").lean();
 
     if (!user) {
       return next(errorHandler(404, "User not found"));
     }
 
-    const { password, ...rest } = user._doc;
-
-    return res.status(200).json(rest);
+    return res.status(200).json(user);
   } catch (error) {
     next(error);
   }
