913e0d02330452971e72189bd8e07b5e8c4718ea
Rola Houhou <rola.h.houhou@gmail.com>
2026-07-09 16:19:50 +0200

    Secure listing routes

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,4 @@
 import express from "express";
-import { verifyToken } from "../utils/verifyUser.js";
 import {
   createListing,
   deleteListing,
@@ -7,13 +6,19 @@ import {
   getListing,
   getListings,
 } from "../controllers/listing.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
+import { validateObjectId } from "../utils/validateObjectId.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
-router.post("/delete/:id", verifyToken, deleteListing);
-router.post("/update/:id", verifyToken, updateListing);
-router.get("/get/:id", getListing);
+
+router.delete("/delete/:id", verifyToken, validateObjectId("id"), deleteListing);
+
+router.post("/update/:id", verifyToken, validateObjectId("id"), updateListing);
+
+router.get("/get/:id", validateObjectId("id"), getListing);
+
 router.get("/get", getListings);
 
 export default router;
